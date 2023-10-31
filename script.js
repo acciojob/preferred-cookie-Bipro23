3d0b1ed7180197336c29a1e6c74a89ebde55a1a3
Bipro23 <88981554+Bipro23@users.noreply.github.com>
2023-10-31 13:42:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-//your JS code here. If required.
-const app = require("./index.js");
-app.listen(3000, () => {
-  console.log('server started');
-});
+const fontSizeInput = document.getElementById("fontsize")
+const fontColorInput = document.getElementById("fontcolor")
+
+
+function set(){
+  let fValue = fontSizeInput.value;
+  let cValue = fontColorInput.value;
+
+  let cookieString = fValue + '=' + cValue;
+  let sCookie = cookieString.split("=")
+document.cookie = sCookie;
+
+  document.body.style.fontSize = `${sCookie[0]}px`;
+  document.body.style.color = sCookie[1];
+}
+document.addEventListener('submit', (event)=>{
+	event.preventDefault();
+	set();
+})
